068434dd409e41cf0ae014b926cc24b94d6f7c5f
Nad Hr <epohster@gmail.com>
2017-01-18 08:09:23 +0200

    8 => solved

---
+++ 10Strings/08. Replace tags/08ReplaceTags.js
@@ -0,0 +1,11 @@
+function replaceTags(args){
+    let str = args[0];
+    let result = str.replace(/(<a href=\")(.*?)(\">)(.*?)(<\/a>)/g, "[$4]($2)"); //$4 takes the 4th match group, $2 takse the 2nd match group
+    
+    console.log(result);
+}
+
+// ZERO TESTS
+replaceTags([ '<p>Please visit <a href="http://academy.telerik.com">our site</a> to choose a training course. Also visit <a href="www.devbg.org">our forum</a> to discuss the courses.</p>' ]);
+
+//RegEx is from C#2 forum
