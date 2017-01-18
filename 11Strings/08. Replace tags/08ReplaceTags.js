36b85146d89a84cea69995aeaa397f0c71d28eea
Nad Hr <epohster@gmail.com>
2017-01-18 08:35:18 +0200

    8 => solved

--- 11Strings/08. Replace tags/08ReplaceTags.js
+++ 11Strings/08. Replace tags/08ReplaceTags.js
@@ -1,6 +1,6 @@
 function replaceTags(args){
     let str = args[0];
-    let result = str.replace(/(<a href=\")(.*?)(\">)(.*?)(<\/a>)/g, "[$4]($2)"); //$4 takes the 4th match group, $2 takse the 2nd match group
+    let result = str.replace(/(<a href=\")(.*?)(\">)(.*?)(<\/a>)/g, "[$4]($2)"); //$4 takes the 4th match group, $2 takes the 2nd match group
     
     console.log(result);
 }
