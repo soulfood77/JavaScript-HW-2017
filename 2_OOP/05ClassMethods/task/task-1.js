c26380c0c3d5fadb1428feae667786615d4b84cd
Nad Hr <epohster@gmail.com>
2017-02-09 11:30:57 +0200

    HW5 - Mocha 13/13 (needs some optimisation)

--- 2_OOP/05ClassMethods/task/task-1.js
+++ 2_OOP/05ClassMethods/task/task-1.js
@@ -50,35 +50,62 @@ class LinkedList {
         return this;
     }
     insert(i, ...values) {
-        if (i === this._length - 1){
-            this.append(...values);
+        if (i === 0) {
+            this.prepend(...values);
             return this;
         }
+        // find locations where the new list will be inserted
         let currentNode = this._head;
         let previousNode;
         for (let j = 0; j < i; j += 1) {
             previousNode = currentNode;
             currentNode = currentNode._next;
         }
+        // create new list
         let listToInsert = new LinkedList;
         listToInsert.append(...values);
-        
+
+        // attach start of new list to previous node and end of new list to current node
         previousNode._next = listToInsert._head;
         listToInsert.findLast()._next = currentNode;
 
         this._length += listToInsert._length;
         return this;
     }
-    at(i, value) {
-
+    at(index, value) {
+        let i = 0;
+        let currentNode = this._head;
+        for (i; i < index; i += 1) {
+            currentNode = currentNode._next;
+        }
+        if (value !== undefined) {
+            return currentNode._value = value;
+        }
+        return currentNode._value;
     }
-
     removeAt(index) {
-
+        if(index === 0){
+            let headValue = this._head._value;
+            let newHead = this._head._next;
+            this._head._value = newHead._value;
+            this._head._next = newHead._next;
+            this._length -= 1;
+            return headValue;
+        }
+        let i = 0;
+        let previousNode;
+        let currentNode = this._head;
+        for (i; i < index; i += 1) {
+            previousNode = currentNode;
+            currentNode = currentNode._next;
+        }
+        previousNode._next = currentNode._next;
+        this._length -= 1;
+        return currentNode._value;
     }
     *[Symbol.iterator]() {
         let current = this._head;
-        for (let i = 0; i < this._length - 1; i += 1) {
+        for (let i = 0; i < this.length - 1; i += 1) {
             if (i === 0) {
                 yield current._value;
             }
@@ -102,22 +129,50 @@ module.exports = LinkedList;
 
 // TESTS
 
-const list1 = new LinkedList();
-list1.append(1, 2, 3, 4, 5, 6).append(7, 8);
-let test1 = list1.toArray();
-list1.prepend(-1, 0);
-list1.insert(1, 'Pepi');
-list1.insert(5, 'Mama', 'mia', 'let', 'me', 'go');
-console.log(list1.toString());
+// const list1 = new LinkedList();
+// list1.append(1, 2, 3, 4, 5, 6).append(7, 8);
+// let test1 = list1.toArray();
+// list1.prepend(-1, 0);
+// list1.insert(1, 'Pepi');
+// list1.insert(5, 'Mama', 'mia', 'let', 'me', 'go');
+// console.log(list1.toString());
 
-const list2 = new LinkedList();
-list2.append(1, 2, 6, 7, 8);
-list2.insert(2, 3, 4);
-list2.insert(4, 5);
-console.log(list2.toString());
+// const list2 = new LinkedList();
+// list2.append(1, 2, 6, 7, 8);
+// list2.insert(2, 3, 4);
+// list2.insert(4, 5);
+// console.log(list2.toString());
 
-const list3 = new LinkedList().append(1, 2).insert(0, 3, 4);
-list3.insert(list3.length - 1, 'kremikovci');
-console.log(list3.toString());
+// TEST INSERT
+// const list3 = new LinkedList().append(1, 2).insert(0, 3, 4);
+// let test = list3.length - 1;
+// list3.insert(list3.length - 1, 'kremikovci');
+// console.log(list3.toString());
 
-console.log('The end');
+// TEST AT
+// const list4 = new LinkedList();
+// list4.append(1, 2, 3, 4, 5, 6);
+// let test4 = list4.at(2)
+// console.log(test4); // 3
+// list4.at(2, 'gosho');
+// console.log(list4.at(2)); // gosho
+
+// TEST REMOVE AT
+// const list5 = new LinkedList();
+// list5.append(1, 2, 3, 4, 5);
+// const removed = list5.removeAt(1); // removed should be 2 and the list should contain 1, 3, 4, 5
+// console.log(removed.toString());
+// console.log(list5.toString());
+
+// TEST REMOVE AT
+// const theObj = { value: 'val', message: 'hello' };
+// const values = ['test', true, null, 1, 2, 'testtest', theObj, 'gg'],
+//     list6 = new LinkedList().append(...values),
+//     removed1 = list6.removeAt(1),
+//     removed2 = list6.removeAt(1),
+//     removed3 = list6.removeAt(0),
+//     removed4 = list6.removeAt(list6.length - 1);
+
+// console.log(removed4);
+// console.log(list6.toString());
+// console.log('The end');
