a56ea5012f5aa5b4866aa4127df1915487aa0f21
Nad Hr <epohster@gmail.com>
2017-02-21 18:07:31 +0200

    Solution Cuki at workshop w. comments

--- 2_OOP/ExamPractice-Jul15/tasks/solution.js
+++ 2_OOP/ExamPractice-Jul15/tasks/solution.js
@@ -1,18 +1,213 @@
 function solve() {
+	// ID GENERATOR
+	const getNextId = (function () {
+		let id = 0;
+		return function () {
+			id += 1
+			return id;
+		};
+	}());
+
+	function validateString(str, min, max) {
+		if (typeof str !== 'string' || str === '') {
+			throw 'Must provide non-empty string';
+		}
+		else if (min !== undefined && max !== undefined) {
+			validateNumberRange(str.length, min, max);
+		}
+	}
+	function validateISBN(isbn) {
+		let test1 = isbn.length;
+		if (typeof isbn !== 'string' || isbn.length !== 10 && isbn.length !== 13) {
+			throw 'ISBN must be a string and 10 or 13 digits long';
+		}
+		if (!/^\d+$/.test(isbn)) {
+			throw 'ISBN must contain only digits';
+		}
+	}
+	function validateNumberRange(number, min, max){
+		if(typeof number !== 'number'){
+			throw 'Must provide a number';
+		}
+		if(number <= min || number > max){
+			throw `Min (${min}) or max (${max}) size exceeded`;
+		}
+	}
+	class Item {
+		constructor(name, description) {
+			this.name = name;
+			this.description = description;
+			this._id = getNextId();
+		}
+		get name() {
+			return this._name;
+		}		
+		set name(name) {
+			validateString(name, 2, 40);
+			this._name = name;
+		}
+		get description() {
+			return this._description;
+		}
+		set description(description) {
+			validateString(description);
+			this._description = description;
+		}
+		get id() {
+			return this._id;
+		}
+	}
+	class Book extends Item {
+		constructor(name, isbn, genre, description) {
+			super(name, description);
+			this.isbn = isbn;
+			this.genre = genre;
+		}
+		get isbn() {
+			return this._isbn;
+		}
+		set isbn(isbn) {
+			validateISBN(isbn);
+			this._isbn = isbn;
+		}
+		get genre() {
+			return this._genre;
+		}
+		set genre(genre) {
+			validateString(genre, 2, 20);
+			this._genre = genre;
+		}
+	}
+	class Media extends Item {
+		constructor(name, rating, duration, description) {
+			super(name, description);
+			this.duration = duration;
+			this.rating = rating;
+		}
+		get duration(){
+			return this._duration;
+		}
+		set duration(duration){
+			validateNumberRange(duration, 0);
+			this._duration = duration;
+		}
+		get rating(){
+			return this._rating;
+		}
+		set rating(rating){
+			validateNumberRange(rating, 1, 5);
+			this._rating = rating;
+		}
+	}
+	class Catalog {
+		constructor(name) {
+			this.name = name;
+			this._items = [];
+			this._id = getNextId();
+		}
+		get name(){
+			return this._name;
+		}
+		set name(name){
+			validateString(name, 2, 40);
+			this._name = name;
+		}
+		add(...items) {
+			if(Array.isArray(items[0])){
+				items = items[0];
+			}
+			if(items.length === 0){
+				throw 'No items were passed';
+			}
+
+			items.forEach(item => {
+				if(item !== 'object'){
+					throw 'All items must be objects';
+				}
+				validateString(item.name, 2, 40);
+				validateString(item.description);
+				validateNumberRange(item.id, 0);
+			});
+
+			this._items.push(...items);
+
+			return this;
+		}
+		find(id) {
+			if(typeof id !== 'number'){
+				throw 'Must provide an ID number';
+			}
+			return this._items.find(x => x.id === id) || null;			
+		}
+		find(options) {
+
+		}
+		search(pattern) {
+
+		}
+	}
+	class BookCatalog extends Catalog {
+		constructor(name) {
+			super(name);
+		}
+		add(...items) {
+
+		}
+		add(itemArr) {
+
+		}
+		getGenres() {
+
+		}
+		find(options) {
+
+		}
+	}
+	class MediaCatalog extends Catalog {
+		constructor(name) {
+			super(name);
+		}
+		add(...items) {
+
+		}
+		add(mediaArr) {
+
+		}
+		getTop(count) {
+
+		}
+		getSortedByDuration() {
+
+		}
+		find(options) {
+
+		}
+	}
+
 	return {
 		getBook: function (name, isbn, genre, description) {
 			// return a book instance
+			return new Book(name, isbn, genre, description);
 		},
 		getMedia: function (name, rating, duration, description) {
 			// return a media instance
+			return new Media(name, rating, duration, description);
 		},
 		getBookCatalog: function (name) {
 			// return a book catalog instance
+			return new BookCatalog(name);
 		},
 		getMediaCatalog: function (name) {
 			// return a media catalog instance
+			return new MediaCatalog(name);
 		}
 	};
 }
 
 module.exports = solve;
+
+const test1 = solve();
+const book1 = test1.getBook('Fahrenheit 451', '9781439142', 'Science Fiction', 'Books are outlawed and firemen burn them')
+const media1 = test1.getMedia('Life Is Beautiful', 4, 116, 'When an open-minded Jewish librarian and his son become victims of the Holocaust, he uses a perfect mixture of will, humor and imagination to protect his son from the dangers around their camp.')
+const 
+console.log('Beauty is in the eye of the beholder. The end!');
---
+++ 2_OOP/Workshop-31Jan/task/solution-Cuki.js
@@ -0,0 +1,186 @@
+// SOLUTION CUKI
+
+function solve() {
+	function getProduct(productType, name, price) {		
+		return {
+			name: name,
+			price: price,
+			productType: productType
+		};
+	}
+
+	function getShoppingCart() {
+		let products = [];
+
+		function add(product) {
+			// TODO: validations
+			// Can be used withot this keyword because not in object/class and products is accessible
+			// Use getProduct to benefit from validations of getProduct method (if any)
+			this.products.push(getProduct(product.productType, product.name, product.price));
+			return this;
+		}
+		function remove(product) {
+			let pIndex = this.products.findIndex(p =>
+				p.name === product.name &&
+				p.price === product.price &&
+				p.productType === product.productType);
+			if (pIndex < 0) {
+				throw 'The product does not exist in this cart';
+			}
+			this.products.splice(pIndex, 1);
+			return this;
+		}
+		function showCost() {
+			// REDUNDANT
+			// if (this.products.length < 1) {
+			// 	return 0;
+			// }
+			// cost = this.prducts.reduce causes an error!
+			let cost = products.reduce((cost, product) => cost + product.price, 0);
+			return cost;
+		}
+		function showProductTypes() {
+			let types = [];
+			if (this.products.length < 1) {
+				return types;
+			}
+
+			// MY SOLUTION
+			// for (let product of this.products) {
+			// 	let checkType = types.findIndex(p => p === product.productType);
+			// 	if (checkType === -1) {
+			// 		types.push(product.productType);
+			// 	}
+			// }
+			// types.sort();			
+			// return types;
+
+			// LOOPS SOLUTION
+			// products.forEach(function(p){
+			// 	if(types.indexOf(p.productType) < 0){
+			// 		types.push(p.productType);
+			// 	}
+			// });
+			// return types;
+
+			//PREDICATES SOLUTION
+			// return products.map(p => p.productType)
+			// 	.sort()
+			// 	.filter((p, i, ps) => i === 0 || p !== ps[i-1]); // after sort all repeating are next to each other. Zero is to make sure we don't go to index -1
+
+			//ASSOCIATIVE ARRAY SOLUTION
+			const uniqTypesObj = {};
+			products.forEach(p => uniqTypesObj[p.productType] = true);
+
+			return Object.keys(uniqTypesObj).sort(); // Returns index values
+
+			// SOLUTION WITHOUT using Object.keys
+			// const uniqTypes = [];
+			// for(const type in uniqTypesObj){
+			// 	uniqTypes.push(type);
+			// }
+			// return uniqTypes.sort();
+
+		}
+		function getInfo() {
+			// ANOTHER SOLUTION
+			// const uniqNames = products.map(p => p.name)
+			// 	.sort()
+			// 	.filter((p, i, ps) => i === 0 || p !== ps[i - 1])
+			// 	.map(name => {
+			// 		const withThisName = products.filter(p => p.name === name);
+			// 		return {
+			// 			name: name,
+			// 			quantity: withThisName.length,
+			// 			totalPrice: withThisName.reduce((cost, product) => cost + product.price, 0) //ERROR
+			// 		}
+			// 	}); 
+
+			const groupedByName = {};
+			products.forEach(p => {
+				if (groupedByName.hasOwnProperty(p.name)) {
+					groupedByName[p.name].quantity += 1;
+					groupedByName[p.name].totalPrice += p.price;
+				}
+				else {
+					groupedByName[p.name] = {
+						name: p.name,
+						quantity: 1,
+						totalPrice: p.price
+					};
+					groupedByName[p.name].quantity = 1;
+					groupedByName[p.name].totalPrice = p.price;
+				}
+			});
+			//
+			const groups = Object.keys(groupedByName).sort()
+				.map(n => {
+					return {
+						name: n,
+						quantity: groupedByName[n].quantity,
+						totalPrice: groupedByName[n].totalPrice
+					}
+				})
+
+			return {
+				// experimental not working everywhere: Object.values(groupedByName).sort(x => x.name),
+				products: groups,
+				totalPrice: showCost()
+			};
+		}
+
+		return {
+			products: products,
+			add: add,
+			remove: remove,
+			showCost: showCost,
+			showProductTypes: showProductTypes,
+			getInfo: getInfo
+		};
+	}
+
+	return {
+		getProduct: getProduct,
+		getShoppingCart: getShoppingCart
+	};
+}
+
+module.exports = solve();
+
+// TESTS
+
+// let MyShop = solve();
+// let MyCart = MyShop.getShoppingCart();
+// let Shokolad = MyShop.getProduct("Sweets", "Shokolad Milka", 2);
+// let Torta = MyShop.getProduct("Sweets", "Tiramisu", 4);
+// let Zahar = MyShop.getProduct("Sweets", "Kristal", 1);
+// let Sol = MyShop.getProduct("Salty", "Pomorie", 1);
+// let Banichka = MyShop.getProduct("Salty", "Sas sirene", 3);
+
+// MyCart.add(Shokolad);
+// MyCart.add(Shokolad);
+// MyCart.add(Torta);
+// MyCart.add(Zahar);
+// MyCart.add(Sol);
+
+//MyCart.remove(Shokolad);
+// let cost = MyCart.showCost();
+// console.log(cost);
+
+// let types = MyCart.showProductTypes();
+// console.log(types);
+
+// let info = MyCart.getInfo();
+
+// console.log('And they lived happily ever afer');
+
+
+// EXAMPLE PREDICATES
+
+// const numbers = [1,2,3,4,5,6,7,8,9];
+
+// function isOdd(x){
+// 	return x% 2 !==0;
+// }
+// numbers.filter(isOdd);
+// console.log(numbers);
