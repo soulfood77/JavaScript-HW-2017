b71ced7cfc07a5135cb0aea72174f009010f62f3
Nad Hr <epohster@gmail.com>
2017-03-12 17:49:37 +0200

    HW1 - Mocha 10/17 (exceptions ok)

--- 3_DOM/06DomOperations/tasks/task-1.js
+++ 3_DOM/06DomOperations/tasks/task-1.js
@@ -16,9 +16,66 @@ Create a function that takes an id or DOM element and an array of contents
   * Any of the contents is neight `string` or `number`
     * In that case, the content of the element **must not be** changed   
 */
-
 module.exports = function () {
-
+  const VALIDATOR = {
+    isDefined: function (arg) {
+      if (arg === undefined) {
+        throw 'Function argument is missing / undefined';
+      }
+      return true;
+    },
+    isString: function (str) {
+      if (typeof str !== 'string') {
+        throw 'Not a string';
+      }
+      if (str === '') {
+        throw 'Empty string not valid';
+      }
+      return true;
+    },
+    isNumber: function (num) {
+      if (typeof num !== 'number') {
+        throw 'Not a number';
+      }
+    },
+    isArray: function (arr) {
+      if (!Array.isArray(arr)) {
+        throw 'Not an array';
+      }
+    },
+    strOrDom: function (arg) {
+      if (VALIDATOR.isString(arg)) {
+        const result = document.getElementById(arg);
+        if (result === null){
+          throw 'No such Id';
+        }
+        return result;
+      }
+      else if (arg instanceof HTMLElement) {
+        return arg;
+      }
+      throw 'Not a string or DOM element';
+    }
+  }
   return function (element, contents) {
+    VALIDATOR.isDefined(element);
+    // if an id is provided, select the element
+    element = VALIDATOR.strOrDom(element);
+
+    VALIDATOR.isDefined(contents);
+    VALIDATOR.isArray(contents);
+    contents.forEach(item => VALIDATOR.isString(item) || VALIDATOR.isNumber(item));
+
+    const frag = document.createDocumentFragment();
+    const div = document.createElement('div');
+    contents.forEach(item => {
+      div.innerText = item;
+      frag.appendChild(div.cloneNode(true));
+    });
+    element.innerHTML = '';
+    element.appendChild(frag);
   };
 };
+
+//TEST VALIDATION
+//test('root', ['mam', 'pap', 'String']);
