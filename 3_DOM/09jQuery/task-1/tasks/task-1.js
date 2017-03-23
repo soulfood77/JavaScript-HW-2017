0ba2b342234e6dec569056326730f9c63406331e
Nad Hr <epohster@gmail.com>
2017-03-23 13:57:18 +0200

    HW4 - Mocha 4/5 (gave up, asked help in forum)

--- 3_DOM/09jQuery/task-1/tasks/task-1.js
+++ 3_DOM/09jQuery/task-1/tasks/task-1.js
@@ -1,29 +1,24 @@
 function solve() {
 
-  // must fix
   return function (selector) {
 
-    // MAIN
     var $mainDiv = $("<div>")
       .addClass("dropdown-list");
 
-    // SELECT
     var $select = $(selector)
       .hide()
       .appendTo($mainDiv)
 
-    // CURRENT
     $("<div>")
       .addClass("current")
-      .attr("data-value", "")
+      .attr("data-value", $select.children().first().val())
       .html($select.children().first().html())
       .click(function () {
         $options.toggle();
-        $(this).html("Select an option");
+        $(this).html("Select a value");
       })
       .appendTo($mainDiv);
 
-    // OPTIONS
     var $options = $("<div>")
       .addClass("options-container")
       .css('position', 'absolute')
@@ -31,28 +26,25 @@ function solve() {
       .on("click", ".dropdown-item", makeSelection)
       .appendTo($mainDiv);
 
-    // ITEMS
     $select
-      .children()
+      .children() 
       .each(function (i) {
         $("<div>")
           .addClass("dropdown-item")
-          .attr("data-value", $(this).attr('value'))
+          .attr("data-value", $(this).val())
           .attr("data-index", i)
           .html($(this).html())
           .appendTo($options);
       });
 
-
     function makeSelection(event) {
       $(".current")
         .html($(this).html())
         .attr("data-value", $(this).attr("data-value"));
-      $options.hide();
+      $options.hide();      
     }
 
     $("body").append($mainDiv);
-
   };
 }
 
