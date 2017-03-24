8cf6afe9674b1624509979e0abe84700fb216de0
Nad Hr <epohster@gmail.com>
2017-03-24 23:48:58 +0200

    Task 1. Table - done (with help from forum)

--- 3_DOM/10Handlebars/tasks/task-1.js
+++ 3_DOM/10Handlebars/tasks/task-1.js
@@ -3,22 +3,30 @@
 function solve() {
 
   return function (selector) {
-    var template = '<table>{{}}</table>'; 
-    
-    /* insert the template here as a string
-    example:
-    var template =
-      '<ul>' +
-      '{{#students}}' +
-      '<li>' +
-      '{{name}}' +
-      '</li>' +
-      '{{/students}}' +
-      '</ul>';
-    */
+    var template = '<table class="items-table">' +
+        '<thead>'+
+          '<tr>' +
+              '<th>#</th>'+
+              '{{#each headers}}' +
+              '<th>{{this}}</th>' +
+              '{{/each}}' +
+          '</tr>'+
+        '</thead>' +
+        '<tbody>' +
+          '{{#each items}}'+
+            '<tr>' +
+              '<td>{{@index}}</td>' +
+              '<td>{{col1}}</td>'+
+              '<td>{{col2}}</td>'+
+              '<td>{{col3}}</td>'+
+            '</tr>'+
+          '{{/each}}'+
+        '</tbody></table>';
 
     $(selector).html(template);
   };
 };
 
 module.exports = solve;
+
+// Help from lllevski & Daniela Popova
