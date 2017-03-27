af6c3fc838550b79ff0b2390b919db79a21a1c37
Nad Hr <epohster@gmail.com>
2017-03-27 23:11:53 +0300

    BGCoder 100/100 handlebars Academy Navigation

--- 3_DOM/ExamPreparation/22-August-2016/task-3/task/task-3.js
+++ 3_DOM/ExamPreparation/22-August-2016/task-3/task/task-3.js
@@ -1,8 +1,31 @@
 function solve() {
     return function() {
         var template = `
-            <!-- your 
-            code here -->
+    <header id="main-header">
+        <ul class="nav">
+        {{#if logo}}
+            <li class="nav-item logo">
+                {{#logo}}
+                    <a href="{{url}}"><img src="{{image}}"></a>
+                {{/logo}}
+            </li>
+        {{/if}}
+        {{#items}}
+            <li class="nav-item">
+                <a href="{{url}}">{{title}}</a>
+                {{#if items}}
+                    <ul class="subnav">
+                    {{#items}}
+                        <li class="nav-item">
+                            <a href="{{url}}">{{title}}</a>
+                        </li>
+                    {{/items}}
+                    </ul>
+                {{/if}}
+            </li>            
+        {{/items}}
+        </ul>
+    </header>
         `;
         return template;
     };
