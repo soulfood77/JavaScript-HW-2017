77408d43f3b4e36edf00c81a3db8c9b3062b79bb
Nad Hr <epohster@gmail.com>
2017-03-27 13:00:57 +0300

    jQuery clickable tabs - done

--- 3_DOM/ExamPreparation/Sample-exam-1/task-2/task-files/scripts.js
+++ 3_DOM/ExamPreparation/Sample-exam-1/task-2/task-files/scripts.js
@@ -1,3 +1,44 @@
-$.fn.tabs = function () {
+$.fn.tabs = function() {
+    const $container = $("#tabs-container");
+    $container.addClass("tabs-container");
 
+    const $tabsContent = $(".tab-item-content");
+    $tabsContent.hide();
+
+    const $allTabTitles = $(".tab-item-title");
+
+    // Initial state
+    let $currentTab = $allTabTitles.first();
+    $currentTab
+        .parent()
+        .addClass("current")
+        .find(".tab-item-content")
+        .show();
+
+    $container.click(setNewCurrent)
+
+    function setNewCurrent(ev) {
+        const $clickedTarget = $(ev.target);
+        if ($clickedTarget.is(".tab-item-title")) {
+            hideOld($currentTab);
+            showNew($clickedTarget);
+            $currentTab = $clickedTarget;
+        }
+    }
+
+    function hideOld(element) {
+        element
+            .parent()
+            .removeClass("current")
+            .find(".tab-item-content")
+            .hide();
+    }
+
+    function showNew(element) {
+        element
+            .parent()
+            .addClass("current")
+            .find(".tab-item-content")
+            .show();;
+    }
 };
