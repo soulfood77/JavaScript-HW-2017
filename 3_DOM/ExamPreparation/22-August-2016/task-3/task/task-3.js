8f43d5cfd57d2a81c666bf836dcf206396dc783e
Nad Hr <epohster@gmail.com>
2017-03-27 17:22:34 +0300

    Renamed author's solutions

--- 3_DOM/ExamPreparation/22-August-2016/task-3/task/task-3.js
+++ 3_DOM/ExamPreparation/22-August-2016/task-3/task/task-3.js
@@ -1,30 +1,9 @@
 function solve() {
     return function() {
         var template = `
-<ul class="nav">
-	{{#if logo}}
-		<li class="nav-item logo">
-			<a href="{{logo.url}}">
-				<img src="{{logo.image}}" />
-			</a>
-		</li>
-	{{/if}}	
-	{{#each items}}
-		<li class="nav-item">
-			<a href="{{url}}">{{title}}</a>
-			
-			{{#if items}}
-				<ul class="subnav">
-					{{#each items}}
-						<li class="nav-item">
-							<a href="{{url}}">{{title}}</a>
-						</li>
-					{{/each}}
-				</ul>
-			{{/if}}
-		</li>
-	{{/each}}
-</ul>`;
+            <!-- your 
+            code here -->
+        `;
         return template;
     };
 }
