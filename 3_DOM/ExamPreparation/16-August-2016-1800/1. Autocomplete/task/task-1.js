9aa16a4b195236634ed5d4beb8549242b9a6fdfc
Nad Hr <epohster@gmail.com>
2017-03-28 14:13:09 +0300

    Mocha 4/5 still works ok in browser Maybe some problem with fat arrow functions

--- 3_DOM/ExamPreparation/16-August-2016-1800/1. Autocomplete/task/task-1.js
+++ 3_DOM/ExamPreparation/16-August-2016-1800/1. Autocomplete/task/task-1.js
@@ -9,59 +9,51 @@ function solve() {
         var listSuggestions = document.querySelector(".suggestions-list");
 
         var uniq = [];
+        var sugExist = initialSuggestions !== undefined && initialSuggestions.length > 0;
+
+        if (sugExist) {
+            // Remove duplicating items
+            for (var i = 0; i < initialSuggestions.length; i += 1) {
+                var itemLow = initialSuggestions[i].toLowerCase();
+                var uniqLow = uniq.map(val => val.toLowerCase());
+                if (uniqLow.indexOf(itemLow) === -1) {
+                    uniq.push(initialSuggestions[i]);
+                }
+            }
+            // Load initial suggestions list because zero test wants it!
+            getList();
+        }
 
         inField.addEventListener("input", loadSuggestions);
         addBtn.addEventListener("click", addNewSuggestion);
         listSuggestions.addEventListener("click", fillSuggestion);
 
+        function getList() {
+            listSuggestions.innerHTML = "";
+            uniq.forEach(sug => {
+                listSuggestions.innerHTML += "<li class='suggestion' style='display:none'><a href='#' class='suggestion-link'>" + sug + "</a></li>";
+            })
+        }
+
         function loadSuggestions(event) {
             var inputTxt = event.target.value;
-            listSuggestions.innerHTML = "";
 
-            if (inputTxt && initialSuggestions !== undefined && initialSuggestions.length > 0) {
-
-                // Remove duplicating items
-                for (var i = 0; i < initialSuggestions.length; i += 1) {
-                    var itemLow = initialSuggestions[i].toLowerCase();
-                    var uniqLow = uniq.map(val => val.toLowerCase());
-                    if (uniqLow.indexOf(itemLow) === -1) {
-                        uniq.push(initialSuggestions[i]);
-                    }
-                }
-
-                // Create all suggestions list
-                uniq.forEach(sug => {
-                    listSuggestions.innerHTML += "<li class='suggestion'><a href='#' class='suggestion-link'>" + sug + "</a></li>";
-                })
-
+            if (inputTxt && sugExist) {
+                // Reload all suggestions list
+                getList();
                 // Filter suggestions on input
-                var foundSuggestion = uniq
-                    .filter(sug => sug
-                        .toLowerCase()
-                        .includes(inputTxt.toLowerCase()));
-
-                var sugList = document
+                document
                     .querySelectorAll(".suggestion")
                     .forEach(sug => {
-                        console.log(inputTxt);
-                        console.log(sug.innerText.toLowerCase().includes(inputTxt.toLowerCase()));
-
+                        console.log(sug.innerText);
                         if (!sug.innerText.toLowerCase().includes(inputTxt.toLowerCase())) {
+                            console.log("hiding", sug.innerText);
                             sug.style.display = "none";
                         } else {
+                            console.log("showing", sug.innerText);
                             sug.style.display = "block";
                         }
                     });
-
-                // if (foundSuggestion.length) {
-                //     for (var i = 0; i < sugList.length; i += 1) {
-                //         for (var j = 0; j < foundSuggestion.length; j += 1) {
-                //             if (sugList[i].innerText === foundSuggestion[j]) {
-                //                 sugList.style.display = "none";
-                //             }
-                //         }
-                //     }
-
             }
         }
 
