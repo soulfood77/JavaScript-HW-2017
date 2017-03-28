f930478d941a7fbfffdb9b859f9610c5a82ea0a8
Nad Hr <epohster@gmail.com>
2017-03-28 13:32:47 +0300

    Mocha 2/5 but seems to work fine in browser

--- 3_DOM/ExamPreparation/16-August-2016-1800/1. Autocomplete/task/task-1.js
+++ 3_DOM/ExamPreparation/16-August-2016-1800/1. Autocomplete/task/task-1.js
@@ -8,6 +8,8 @@ function solve() {
         var addBtn = document.querySelector(".btn-add");
         var listSuggestions = document.querySelector(".suggestions-list");
 
+        var uniq = [];
+
         inField.addEventListener("input", loadSuggestions);
         addBtn.addEventListener("click", addNewSuggestion);
         listSuggestions.addEventListener("click", fillSuggestion);
@@ -16,37 +18,61 @@ function solve() {
             var inputTxt = event.target.value;
             listSuggestions.innerHTML = "";
 
-            // if (inputTxt && initialSuggestions !== undefined && initialSuggestions.length > 0) {
-            //     // check if includes works in BGCoder
-            //     var foundSuggestion = initialSuggestions.filter(sug => sug.toLowerCase().includes(inputTxt.toLowerCase()));
+            if (inputTxt && initialSuggestions !== undefined && initialSuggestions.length > 0) {
 
-            //     if (foundSuggestion.length) {
-            //         foundSuggestion.forEach(sug => {
-            //             listSuggestions.innerHTML += "<li class='suggestion'><a href='#' class='suggestion-link'>" + sug + "</a></li>";
-            //             // var newLi = document.createElement("li");
-            //             // newLi.className = "suggestion";
-            //             // var newA = document.createElement("a");
-            //             // newA.className = "suggestion-link";
-            //             // newA.innerHTML = sug;
-            //             // newLi.appendChild(newA);
-            //             // listSuggestions.appendChild(newLi);
-            //         })
-            //     }
-            // }
+                // Remove duplicating items
+                for (var i = 0; i < initialSuggestions.length; i += 1) {
+                    var itemLow = initialSuggestions[i].toLowerCase();
+                    var uniqLow = uniq.map(val => val.toLowerCase());
+                    if (uniqLow.indexOf(itemLow) === -1) {
+                        uniq.push(initialSuggestions[i]);
+                    }
+                }
+
+                // Create all suggestions list
+                uniq.forEach(sug => {
+                    listSuggestions.innerHTML += "<li class='suggestion'><a href='#' class='suggestion-link'>" + sug + "</a></li>";
+                })
+
+                // Filter suggestions on input
+                var foundSuggestion = uniq
+                    .filter(sug => sug
+                        .toLowerCase()
+                        .includes(inputTxt.toLowerCase()));
+
+                var sugList = document
+                    .querySelectorAll(".suggestion")
+                    .forEach(sug => {
+                        console.log(inputTxt);
+                        console.log(sug.innerText.toLowerCase().includes(inputTxt.toLowerCase()));
+
+                        if (!sug.innerText.toLowerCase().includes(inputTxt.toLowerCase())) {
+                            sug.style.display = "none";
+                        } else {
+                            sug.style.display = "block";
+                        }
+                    });
+
+                // if (foundSuggestion.length) {
+                //     for (var i = 0; i < sugList.length; i += 1) {
+                //         for (var j = 0; j < foundSuggestion.length; j += 1) {
+                //             if (sugList[i].innerText === foundSuggestion[j]) {
+                //                 sugList.style.display = "none";
+                //             }
+                //         }
+                //     }
+
+            }
         }
 
         function addNewSuggestion(event) {
             var inputTxt = inField.value;
-            var isListed = initialSuggestions.some(function(sug) {
-                sug.toLowerCase() === inputTxt.toLowerCase();
-            });
+            var isListed = uniq.some(sug => sug.toLowerCase() === inputTxt.toLowerCase());
 
             if (!isListed) {
-                initialSuggestions.push(inputTxt);
-                initialSuggestions.sort();
+                uniq.push(inputTxt);
+                uniq.sort();
             }
-
-
         }
 
         function fillSuggestion(event) {
@@ -54,6 +80,5 @@ function solve() {
         }
     };
 }
-
 
 module.exports = solve;
