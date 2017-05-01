ab8975a79edba3a89937c1e948ae0c69333b4ba2
Nad Hr <epohster@gmail.com>
2017-05-02 00:25:10 +0300

    Template caching done

--- 4_Apps/Workshop-20Apr-FortuneCookies/public/js/templates.js
+++ 4_Apps/Workshop-20Apr-FortuneCookies/public/js/templates.js
@@ -2,10 +2,17 @@ import {get as getRequst } from 'requester';
 // Doesn't work
 // import { Handlebars } from 'handlebars';
 
+const cacheObj = {};
+
 export function load(templateName) {
+    if (cacheObj.hasOwnProperty(templateName)) {
+        return Promise.resolve(cacheObj[templateName]);
+    }
+
     return getRequst(`templates/${templateName}.handlebars`)
         .then(template => {
             const compiledTemplate = Handlebars.compile(template);
+            cacheObj[templateName] = compiledTemplate;
             return Promise.resolve(compiledTemplate);
         })
 }
