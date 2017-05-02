3f5fd7d9f3f49fc814c27abf0e7d358f32e61fdd
Nad Hr <epohster@gmail.com>
2017-05-02 10:11:31 +0300

    Copy router from previous demo

---
+++ 4_Apps/Workshop-25Apr-SelfManager/public/js/myRouter.js
@@ -0,0 +1,58 @@
+class MyRouter {
+    constructor() {
+        this._routes = [];
+    }
+
+    on(targetUrl, callback) {
+        this._routes.push({
+            targetUrl,
+            callback
+        });
+
+        return this;
+    }
+
+    navigate() {
+        const currentUrl = location.hash.slice(1);
+
+        // Destructuring assignment
+        for (const { targetUrl, callback }
+            of this._routes) {
+            const params = MyRouter.matchUrls(currentUrl, targetUrl);
+            if (params) {
+                callback(params);
+                break;
+            }
+        }
+    }
+
+    // For UT purposes it's better that 
+    // both current and target Urls be passed as params to the function
+    static matchUrls(currentUrl, targetUrl) {
+        const currentUrlParts = currentUrl.split(/\//g);
+        const targetUrlParts = targetUrl.split(/\//g);
+
+        if (targetUrlParts.length !== currentUrlParts.length) {
+            return false;
+        }
+
+        const params = {};
+        const len = currentUrlParts.length;
+        // Loop and extract only parts starting with colon
+        for (var i = 0; i < len; i++) {
+            if (targetUrlParts[i][0] !== ':') {
+                if (currentUrlParts[i] !== targetUrlParts[i]) {
+                    return false;
+                }
+            } else {
+                // Remove colon
+                const paramName = targetUrlParts[i].slice(1);
+                params[paramName] = currentUrlParts[i];
+            }
+        }
+
+        return params;
+    }
+}
+
+export { MyRouter };
