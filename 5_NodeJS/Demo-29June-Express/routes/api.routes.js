4e6b124fca16964935a7e64c5978189a07a95ec6
Nadezhda Hristova <epohster@gmail.com>
2017-07-07 16:13:01 +0300

    Query parameters and paging

--- 5_NodeJS/Demo-29June-Express/routes/api.routes.js
+++ 5_NodeJS/Demo-29June-Express/routes/api.routes.js
@@ -1,20 +1,49 @@
+const { Router } = require('express');
+
 const items = [{
     id: 1,
     name: 'Cuki',
 }];
 
 const attach = (app) => {
-    app.get('/api/items', (req, res) => {
-        res.send(items);
-    });
+    const router = new Router();
 
-    app.post('/api/items', (req, res) => {
-        const item = req.body;
-        item.id = items.length + 1;
-        items.push(item);
-        res.status(201)
-            .send(item);
-    });
+    router
+        .get('/:id', (req, res) => {
+            const id = parseInt(req.params.id, 10);
+            const item = items.find((i) => i.id === id);
+            if (!item) {
+                return res.status(404)
+                    .send({
+                        error: 'Not found',
+                    });
+            }
+            return res.send(item);
+        })
+        .get('/', (req, res) => {
+            let { q, page, size } = req.query;
+            page = parseInt(page, 10) || 1;
+            size = parseInt(size, 10) || 10;
+
+            let result = items;
+            if (q) {
+                q = q.toLowerCase();
+                result = items.filter((item) => {
+                    return item.name.toLocaleLowerCase().includes(q);
+                });
+            }
+            result = result.slice((page - 1) * size, page * size);
+            res.send(result);
+        })
+        .post('/', (req, res) => {
+            const item = req.body;
+            item.id = items.length + 1;
+            items.push(item);
+            res.status(201)
+                .send(item);
+        });
+
+    app.use('/api/items', router);
 };
 
 module.exports = attach;
